21aa7687885dc600ef32a6f0439b8410d99ac276
mayfly-moon <hello@yuukokonagai.com>
2023-05-08 00:58:13 -0700

    testing
    minor changes after the browser testing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log ("Hi! Thank you for visiting my portfolio.");
 
-function menuToggle(){
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-        x.className = 'navtoggle';
-    }
-} 
+
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
